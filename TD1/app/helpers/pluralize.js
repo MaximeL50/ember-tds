7c25cb84668f70352073ab28f873489c6a97e551
Maxime Le Biez <21805596@campus.unicaen.fr>
2019-02-06 17:30:12 +0100

    ajout ex2

---
+++ TD1/app/helpers/pluralize.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+
+export function pluralize(params/*, hash*/) {
+
+  let [count,zero,one,plus]=params;
+  if(count===0){
+    return zero;
+  }
+  if(count===1){
+    return count+" "+one;
+  }
+  return count+" "+plus;
+}
+
+export default helper(pluralize);
