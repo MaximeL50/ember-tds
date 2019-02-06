e522de59e94c48f415ef59e7bf9fedad9204d2bc
Maxime Le Biez <21805596@campus.unicaen.fr>
2019-02-06 14:56:40 +0100

    first commit

---
+++ TD1/app/routes/ex1.js
@@ -0,0 +1,29 @@
+import Route from '@ember/routing/route';
+import  EmberObject, { computed } from '@ember/object';
+
+const Note=EmberObject.extend({
+    content: '',
+    MAX: 100,
+    info : null,
+    size: computed('content',function () {
+      this.set('info',null);
+      return this.MAX-this.content.length;
+    }),
+  style : computed('content',function () {
+    let size=this.get('size');
+    if(size<20){
+      return 'danger';
+    }else if(size<50){
+      return 'warning';
+    }
+    else{
+      return 'info';
+    }
+  })
+  }
+);
+export default Route.extend({
+  model(){
+    return Note.create();
+  }
+});
